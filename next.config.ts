8a92be5312099a1eab4c6e6d0095410fb35a24e0
Dickson Boateng <dicksonboateng67@gmail.com>
2025-12-12 16:00:17 +0000

    Fix vercel build errors 2nd time

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
-  },
-  experimental: {
-    // Disable static optimization for pages with dynamic behavior
-    outputFileTracingIncludes: {},
   },
 };
 
